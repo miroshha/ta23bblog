bf5209df4210e6ac28826cbfcf5aa5ee5dc44bbb
Kaspar Martin Suursalu <kasparsu@gmail.com>
2025-11-28 10:52:05 +0200

    Add admin list view and custom pagination views

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,7 +13,8 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        $posts = Post::paginate();
+        return view('posts.index', compact('posts'));
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\PublicController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PublicController::class, 'index'])->name('home');
 Route::get('/post/{post}', [PublicController::class, 'post'])->name('post');
+
+Route::get('/admin/posts', [PostController::class, 'index'])->name('posts.index');
